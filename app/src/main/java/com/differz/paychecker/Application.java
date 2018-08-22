56e0be991a7bed24e872f8c6df2bce12f26d4101
Sergii Suprun <differ@xlan.net.ua>
2018-08-22 22:49:40 +0300

    add logger

--- app/src/main/java/com/differz/paychecker/Application.java
+++ app/src/main/java/com/differz/paychecker/Application.java
@@ -1,13 +1,17 @@
 package com.differz.paychecker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        System.out.println(Application.class.getPackage().getName());
+        logger.info(Application.class.getPackage().getName());
         SpringApplication.run(Application.class, args);
     }
 }
